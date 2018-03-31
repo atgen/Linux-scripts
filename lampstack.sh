fd67129771901e18c43c3c517ae58f17fea6e463
unknown <dumitru.eugen.gen@gmail.com>
2018-03-31 09:05:27 +0300

    dist added update/upgrade

--- lampstack.sh
+++ lampstack.sh
@@ -3,4 +3,20 @@
 
 clear
 echo "Good Morning World"
-apt get-update
+# We need to update/upgrade dist
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+
+# Disable SELinux
+echo 0 > /selinux/enforce
+
+# Disable appArmor: 1st Stop it and then Disable it
+sudo systemctl stop apparmor
+sudo systemctl disable apparmor
+
+# Installing Apache
+sudo apt-get install apache2 -y
+
+# grep this_word this_file.txt 
+
+
