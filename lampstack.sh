dd99dc51a6d2143a00733b78b7162b6d1257b517
Dumitru Eugen <eu-gen@users.noreply.github.com>
2018-04-30 18:13:46 +0300

    Update lampstack.sh

--- lampstack.sh
+++ lampstack.sh
@@ -15,8 +15,10 @@ sudo systemctl stop apparmor
 sudo systemctl disable apparmor
 
 # Installing Apache
-sudo apt-get install apache2 -y
+sudo apt-get install -y apache2
 
+# Install PHP 7.0 + required libraries 
+sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip
 # grep this_word this_file.txt 
 
 
